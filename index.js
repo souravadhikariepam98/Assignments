fe6fddc8027211d07be9ddde506d1b96c909bd01
souravadhikariepam98 <Sourav_Adhikari@Epam.com>
2022-03-09 13:16:35 +0530

    Client Side Validation in name and contact no using js

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const submitButton=document.getElementsByClassName('submitBtn btn11')[0];
 submitButton.addEventListener("click", myFunction);
 function myFunction() {
     const contactNo=document.getElementById('contactNo').value;
+    const salesPersonName=document.getElementById('salesPersonName').value;
+    let isValid=true;
     if(contactNo.length===10)
-        alert("Form Submitted Successfully!!!");
+        isValid=true;
     else
-        alert("wrong input in Phone number...");
+        isValid=false;
+    
+    if(salesPersonName.length===0 && isValid)
+        isValid=false;
+    else if(isValid)
+        isValid=true;
+
+    if(isValid){
+        alert("Form Submitted Sucessfully!!!");
+    }else{
+        alert("Error in contact no or name");
+    }
     document.getElementById('contactNo').value="";
+    document.getElementById('salesPersonName').value="";
 }
